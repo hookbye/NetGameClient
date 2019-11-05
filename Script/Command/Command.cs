f437a12673b13eba417fa0b17cc0c2588c024ad7
hookbye <weljun@sina.com>
2019-11-05 22:17:35 +0800

    SimpleAI

---
+++ Script/Command/Command.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command {
+    public GameObject target;
+    public float executeTime=0f;
+
+	void ExeCute()
+    {
+        
+    }
+}
