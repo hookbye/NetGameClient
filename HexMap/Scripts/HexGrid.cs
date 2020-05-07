2cfe08548f9c4c4d27f1e1ba7acbff91def14909
hookbye <weljun@sina.com>
2020-05-07 22:25:25 +0800

    hexmap

---
+++ HexMap/Scripts/HexGrid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+    public int width = 6;
+    public int height = 6;
+    public HexCell cellPrefab;
+    public Text celllabelPrefab;
+    Canvas gridCanvas;
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+    }
+}
---
+++ HexMap/Scripts/HexMetrics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HexMetrics {
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+    public static Vector3[] corners =
+    {
+        new Vector3(0f,0f,outerRadius),
+        new Vector3(innerRadius,0f,0.5f*outerRadius),
+        new Vector3(innerRadius,0f,-0.5f*outerRadius),
+        new Vector3(0f,0f,-outerRadius),
+        new Vector3(-innerRadius,0f,-0.5f*outerRadius),
+        new Vector3(-innerRadius,0f,0.5f*outerRadius),
+    };
+    /* shape 0:center 1-6 corner
+    * 
+    *       *1   
+    *    *      *
+    * *6           *2
+    * *     o0     *
+    * *5           *3
+    *    *      *
+    *       *4
+    *         
+    * */
+}
